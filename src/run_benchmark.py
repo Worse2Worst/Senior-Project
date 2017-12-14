355313b2505e165dc0c37e85b6378def7c7878c9
Worse2Worst <wasuwach@hotmail.com>
2017-12-14 09:25:49 +0700

    added 'gen_request' method

--- src/run_benchmark.py
+++ src/run_benchmark.py
@@ -3,9 +3,9 @@ from pdp_lib import draw
 from pdp_lib import preprocess
 from pdp_lib import save_pics
 
-
+'''
 filename='pdp_instances/LiLim/pdp_100/lrc207.txt'
-
-
 nodes= preprocess.read_node(filename)
 draw.draw_original_nodes(nodes)
+'''
+save_pics.save_all_pics()

9132f615c092c7166eaf015cdffd09d2be924097
Worse2Worst <wasuwach@hotmail.com>
2018-03-18 09:13:31 +0700

    listening to Chick Corea's Spain

---
+++ pdp_lib/processing_google_map.py
@@ -0,0 +1,256 @@
+import math
+from itertools import chain
+import numpy as np
+import copy
+
+# Loading a file
+def load_file(filepath):
+    file = open(filepath, 'rt')
+    line = file.readline()
+
+    numVehicles, loadCapacities, speed = [int(num) for num in line.split()]
+
+    # Initialized necessary variables
+    locations = []
+    demands = []
+    timeWindows = []
+    ServiceTimes = []
+    pickupSiblings = []
+    deliverySiblings = []
+    requestType = []
+
+    ######  Read the file ################
+    line = file.readline()
+    while line:
+        index, x, y, demand, earliestTime, latestTime, service_time, p_sibling, d_sibling = [int(num) for num in line.split()]
+        locations.append([x,y])
+        demands.append(demand)
+        timeWindows.append([earliestTime,latestTime])
+        ServiceTimes.append(service_time)
+        pickupSiblings.append(p_sibling)
+        deliverySiblings.append(d_sibling)
+        if (p_sibling == 0):
+            requestType.append('pickup')
+        else :
+            requestType.append('delivery')
+        # Read the next line
+        line = file.readline()
+    # close the file
+    file.close()
+
+    # the depot have unlimit time windows
+    timeWindows[0] = [0,9999999999999999]
+    requestType[0] = 'DEPOT'
+
+    locations = dict(enumerate(locations))
+
+    data = [locations,demands,timeWindows,ServiceTimes,pickupSiblings,deliverySiblings,requestType]
+    return numVehicles, loadCapacities, speed,data
+
+# Generate pairs of requests (pickup-> delivery)
+def generate_request(pickupSiblings,deliverySiblings,requestType):
+    requests = []
+    for i in range(len(requestType)):
+        if(requestType[i]=='pickup'):
+            requests.append((i,deliverySiblings[i]))
+    return dict(enumerate(requests))
+
+
+# calculate Euclidean distances between nodes
+def distance(location1,location2):
+    x1 = location1[0]
+    y1 = location1[1]
+    x2 = location2[0]
+    y2 = location2[1]
+    return math.sqrt(((x1-x2)**2) +((y1-y2)**2))
+
+# Create a table that memo all distances bewtween any 2 nodes in the map
+def createDistanceTable(LOCATIONS):
+    n = len(LOCATIONS)
+    distances = np.zeros((n, n))
+    # create nxn matrix to memo the distances between nodes
+    for i in range(n):
+        for j in range(n):
+            distances[i][j] = distance(LOCATIONS[i], LOCATIONS[j])
+    return distances
+
+# calculate duration of traveling between a pair of nodes
+def duration(v1,v2,DISTANCES,serviceTimes,speed):
+    return (DISTANCES[v1][v2]/speed + serviceTimes[v1])
+
+
+# Create a table that memo all durations of traveling bewtween any 2 nodes in the map
+def createDurationTable(locations, DISTANCES, serviceTimes, speed=1.0):
+    # Sometimes, the speed from instances are 'Zeros', so we will make it 'one'
+    if(speed<=0):
+        speed = 1.0
+
+    n = len(locations)
+    durations = np.zeros((n, n))
+    # create nxn matrix to memo the distances between nodes
+    for i in range(n):
+        for j in range(n):
+            durations[i][j] = DISTANCES[i][j]/speed + serviceTimes[i]
+    durations = np.ndarray.tolist(durations)
+    return durations
+
+
+
+def create_depots(LOCATIONS):
+    DEPOTS = []
+    width = -1
+    if (len(LOCATIONS) >= 1000):
+        width = 500
+    elif (len(LOCATIONS) >= 800):
+        width = 400
+    elif (len(LOCATIONS) >= 600):
+        width = 300
+    elif (len(LOCATIONS) >= 400):
+        width = 200
+    elif (len(LOCATIONS) >= 200):
+        width = 140
+    else:
+        width = 100
+    # Depot-0 , at center
+    dep0 = copy.deepcopy(LOCATIONS[0])
+
+    # Depot-1 , at upper left
+    dep1 = [int(width/4),int(3*width/4)]
+
+    # Depot-2 , at upper right
+    dep2 = [int(3*width / 4), int(3*width / 4)]
+
+    # Depot-3 , at lower left
+    dep3 = [int(width / 4), int(width / 4)]
+
+    # Depot-4 , at lower right
+    dep4 = [int(3*width / 4), int(width / 4)]
+
+    # # Special case for small size map (apporx. 100 nodes) ##
+    # if(len(LOCATIONS)<150):
+    #     dep1[0]-=10
+    #     dep2[0]-=10
+    #     dep3[0]-=10
+    #     dep4[0]-=10
+
+    ## Put every depots into the array ##
+    DEPOTS.append(dep0)
+    DEPOTS.append(dep1)
+    DEPOTS.append(dep2)
+    DEPOTS.append(dep3)
+    DEPOTS.append(dep4)
+    DEPOTS = dict(enumerate(DEPOTS))
+    return DEPOTS
+
+def locations_of_this_depot(dep,REQ_BY_DEPOTS,LOCATIONS):
+    THIS_DEP_REQS = REQ_BY_DEPOTS[dep]
+    THIS_LOCATIONS_INDEX = THIS_DEP_REQS.values()
+    THIS_LOCATIONS_INDEX = list(chain(*THIS_LOCATIONS_INDEX))
+    THIS_DEP_LOCATIONS = {idx: LOCATIONS[idx] for idx in THIS_LOCATIONS_INDEX}
+    return THIS_DEP_LOCATIONS
+
+
+def distances_to_depots(DEPOTS, LOCATIONS):
+    # distances_to_depots[i][j] = location_i to depot_j
+    n = len(LOCATIONS)
+    m = len(DEPOTS)
+    distances_to_depots = np.zeros(shape=(n,m))
+    for i in range(n):
+        for j in range(m):
+            distances_to_depots[i][j] = distance(LOCATIONS[i],DEPOTS[j])
+    return distances_to_depots
+
+def distances_from_depots(DEPOTS, LOCATIONS):
+    # distances_from_depots[i][j] = depots_i to location_j
+    n = len(DEPOTS)
+    m = len(LOCATIONS)
+    distances_from_depots = np.zeros(shape=(n,m))
+    for i in range(n):
+        for j in range(m):
+            distances_from_depots[i][j] = distance(DEPOTS[i],LOCATIONS[j])
+    return distances_from_depots
+
+def requests_by_depots(DEPOTS,REQUESTS,DEPOT_NUMBERS):
+    REQ_BY_DEPOTS = []
+    for i in range(len(DEPOTS)):
+        temp = {}
+        for id, req in REQUESTS.items():
+            if (DEPOT_NUMBERS[id] == i):
+                temp[id] = req
+        REQ_BY_DEPOTS.append(temp)
+    return REQ_BY_DEPOTS
+
+def simple_assign_depots(REQUESTS, LOCATIONS, DEPOTS,DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS):
+    dep_nums = np.zeros(shape=(len(REQUESTS)))
+    n = len(REQUESTS)
+    m = len(DEPOTS)
+    for i in range(n): # loop in requests
+        minDist = 9999999999
+        minDep = 99
+        for j in range(m): # loop in depots
+            distPickup = DISTANCES_FROM_DEPOTS[j][REQUESTS[i][0]]
+            distDelivery = DISTANCES_TO_DEPOTS[REQUESTS[i][1]][j]
+            dist = distPickup + distDelivery
+            if(dist < minDist):
+                minDist = dist
+                minDep = j
+        dep_nums[i] = minDep
+    return dep_nums
+
+def can_route(req1,req2,DISTANCES,DURATIONS,timeWindows):
+    p1 = req1[0]
+    d1 = req1[1]
+    p2 = req2[0]
+    d2 = req2[1]
+    p1_tw = timeWindows[p1]
+    d1_tw = timeWindows[d1]
+    p2_tw = timeWindows[p2]
+    d2_tw = timeWindows[d2]
+    if(d1_tw[0] + DURATIONS[d1][p2] <= p2_tw[1]):
+        return True
+    if (p1_tw[0] + DURATIONS[p1][p2] <= p2_tw[1] and p2_tw[0] + DURATIONS[p2][d1] <= d1_tw[1] and d1_tw[0] + DURATIONS[d1][d2] <= d2_tw[1]):
+        return True
+    return  False
+
+def can_merge_requests(REQUESTS,DISTANCES,timeWindows,DURATIONS,key1,key2):
+    req1 = REQUESTS[key1]
+    req2 = REQUESTS[key2]
+    return can_route(req1,req2,DISTANCES,DURATIONS,timeWindows) or can_route(req2,req1,DISTANCES,DURATIONS,timeWindows)
+
+def worse2worst_assign_depots(REQUESTS, timeWindows,DISTANCES,DURATIONS,DEPOTS,DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS):
+    LOCATIONS = []
+    dep_nums = np.zeros(shape=(len(REQUESTS)))
+    old_dep_nums = simple_assign_depots(REQUESTS, LOCATIONS, DEPOTS,DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS)
+    n = len(REQUESTS)
+    m = len(DEPOTS)
+
+    LT = []
+    for key, value in REQUESTS.items():
+        LT.append((key, timeWindows[value[0]][1]))
+    LT = sorted(LT, key=lambda x: x[1])
+
+    for key1,_ in LT:
+        old_dep = int(old_dep_nums[key1])
+        value = REQUESTS[key1]
+        min_cost = DISTANCES_FROM_DEPOTS[old_dep][value[0]] + DISTANCES_TO_DEPOTS[value[1]][old_dep]
+        # print('Min cost = '+str(min_cost))
+        minDep = old_dep
+        p1 = value[0]
+        d1 = value[1]
+        for key2,val2 in REQUESTS.items():
+            p2 = val2[0]
+            d2 = val2[1]
+            if(key1 != key2 and can_merge_requests(REQUESTS,DISTANCES,timeWindows,DURATIONS,key1,key2)):
+                cost = DISTANCES[p1][p2] + DISTANCES[d1][d2]
+                if(cost < min_cost):
+                    # print('found!!!!')
+                    # print('Cost = ' + str(cost))
+                    min_cost = cost
+                    minDep = old_dep_nums[key2]
+        dep_nums[key1] = minDep
+        old_dep_nums[key1] = minDep
+    return dep_nums
+
+# def KNN_assign_depots(REQUESTS, timeWindows, DEPOTS,DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS):
+
+
---
+++ src/main_google_map.py
@@ -0,0 +1,201 @@
+import time
+import random
+import os
+import pandas as pd
+import csv
+import numpy as np
+from itertools import chain
+from pdp_lib import processing as proc
+from pdp_lib import util
+from GA_lib import GA_multi_depot as GA
+from GA_lib import operation_multi_depot as operation
+from GA_lib import evaluate_multi_depot as evaluate
+
+
+
+
+def solve_and_print(filepath, rounds = 10, population_size = 100, generations=2000, crossoverRate=1.0, mutationRate=0.5):
+    process_time = time.time()
+    numVehicles, LoadCapacities, speed, data = proc.load_file(filepath)
+    LOCATIONS = data[0]
+    DEMANDS = data[1]
+    timeWindows = data[2]
+    serviceTimes = data[3]
+    pickupSiblings = data[4]
+    deliverySiblings = data[5]
+    requestType = data[6]
+    REQUESTS = proc.generate_request(pickupSiblings, deliverySiblings, requestType)
+    DISTANCES = proc.createDistanceTable(LOCATIONS)
+    DURATIONS = proc.createDurationTable(LOCATIONS, DISTANCES, serviceTimes, speed)
+    DEPOTS = proc.create_depots(LOCATIONS)
+    DISTANCES_FROM_DEPOTS = proc.distances_from_depots(DEPOTS, LOCATIONS)
+    DISTANCES_TO_DEPOTS = proc.distances_to_depots(DEPOTS, LOCATIONS)
+    # DEPOT_NUMBERS = proc.simple_assign_depots(REQUESTS, LOCATIONS, DEPOTS, DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS)
+    DEPOT_NUMBERS = proc.worse2worst_assign_depots(REQUESTS, timeWindows,DISTANCES,DURATIONS,DEPOTS,DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS)
+    REQ_BY_DEPOTS = proc.requests_by_depots(DEPOTS, REQUESTS, DEPOT_NUMBERS)
+
+    print('----------------Instance name: ' + str(filepath) + '-----------------------------------')
+    print(" processing time --- %s seconds ---" % (time.time() - process_time))
+
+    total_results = []
+    for round in range(rounds):
+        results_all_depots = []
+        distance_all_depots = []
+        time_all_depots = []
+        for id_depot in range(len(DEPOTS)):
+            THIS_DEP_REQS = REQ_BY_DEPOTS[id_depot]
+            NODES_OF_THIS_DEPOT = proc.locations_of_this_depot(id_depot, REQ_BY_DEPOTS, LOCATIONS)
+            # ############### SOLVING THE PROBLEMS !!!!!!!! ######################################
+            pops_create_time = time.time()
+            calculation_time = time.time()
+            print('#################### Round :' +str(round+1)+' ############################')
+            ## Initialize the populations
+            populations = []
+            for _ in range(population_size):
+                chromosome = GA.initialize_Feasible_chromosome(DISTANCES, DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS, id_depot, DURATIONS, timeWindows, THIS_DEP_REQS, DEMANDS, LoadCapacities)
+                populations.append(chromosome)
+            # print("Populations creation time --- %s seconds ---" % (time.time() - pops_create_time))
+            ## Crossovers and mutate ##
+            GA_time = time.time()
+
+            ## Evaluate fitnesses of all populations ##
+            best_fitness_so_far = -99999999
+            for gen in range(generations):
+                fitness_table = []
+                for chromosome in populations:
+                    fitness_table.append(
+                        evaluate.chromosomeFitness(chromosome, DISTANCES, DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS,
+                                                   id_depot))
+                populations = [x for _, x in sorted(zip(fitness_table, populations), reverse=True)]
+                populations.pop()
+                populations.pop()
+                elite1 = populations.pop(0)
+                elite2 = populations.pop(0)
+
+
+                ## Tournament Selection ##
+                # id1, id2 = random.randrange(0, len(populations)), random.randrange(0, len(populations))
+                id1,id2,id3,id4 = random.sample(range(0, len(populations)), 4)
+                fitness1,fitness2,fitness3,fitness4 = fitness_table[id1],fitness_table[id2],fitness_table[id3],fitness_table[id4]
+                candidate1,candidate2,candidate3,candidate4 = populations[id1], populations[id2],populations[id3],populations[id4]
+                parent1 = operation.tournament_selection(candidate1,candidate2,fitness1,fitness2)
+                parent2 = operation.tournament_selection(candidate3,candidate4,fitness3,fitness4)
+
+                ## Crossing over !! ####
+                child1, child2 = operation.crossover(DISTANCES, DURATIONS, timeWindows, THIS_DEP_REQS, parent1, parent2,
+                                                     DEMANDS, LoadCapacities, DISTANCES_FROM_DEPOTS,
+                                                     DISTANCES_TO_DEPOTS, id_depot, crossoverRate)
+                # if(not evaluate.haveEqualNodes(child1,child2,LOCATIONS)):
+                #         print('note have Equal nodes, Crossover Bug!!!!!'+ str(gen))
+                #         break
+                #
+                ## Mutation ##
+                child1 = operation.mutate(child1, DISTANCES, DURATIONS, timeWindows, THIS_DEP_REQS, DEMANDS,
+                                          LoadCapacities, DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS, id_depot, mutationRate)
+                child2 = operation.mutate(child2, DISTANCES, DURATIONS, timeWindows, THIS_DEP_REQS, DEMANDS,
+                                          LoadCapacities, DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS, id_depot, mutationRate)
+                if (not evaluate.haveEqualNodes(child1, child2, NODES_OF_THIS_DEPOT)):
+                    print('note have Equal nodes, Mutation Bug!!!!!' + str(gen))
+                    break
+
+
+                ## Evaluate Fitness of the best ones ###
+                current_fitness = evaluate.chromosomeFitness(child1, DISTANCES, DISTANCES_FROM_DEPOTS,
+                                                             DISTANCES_TO_DEPOTS, id_depot)
+                current_fitness = max(current_fitness,
+                                      evaluate.chromosomeFitness(child2, DISTANCES, DISTANCES_FROM_DEPOTS,
+                                                                 DISTANCES_TO_DEPOTS, id_depot))
+                current_fitness = max(current_fitness,
+                                      evaluate.chromosomeFitness(elite1, DISTANCES, DISTANCES_FROM_DEPOTS,
+                                                                 DISTANCES_TO_DEPOTS, id_depot))
+                current_fitness = max(current_fitness,
+                                      evaluate.chromosomeFitness(elite2, DISTANCES, DISTANCES_FROM_DEPOTS,
+                                                                 DISTANCES_TO_DEPOTS, id_depot))
+
+                ## Add them back into populations ###
+                populations.append(child1)
+                populations.append(child2)
+                populations.append(elite1)
+                populations.append(elite2)
+
+                ### Check break conditions ####
+                if (current_fitness > best_fitness_so_far):
+                    best_fitness_so_far = current_fitness
+                    bestFitGen = gen
+                    # print('#### New Best Fitness !! , Best so far is :' + str(10000.0 / best_fitness_so_far) + '#####')
+                    # print('#### This Generation: ' + str(gen) + '#######')
+                if (gen - bestFitGen >= 500):
+                    # print('#### Break Generation: ' + str(gen) + '#######')
+                    break
+                # print('############# Round-'+str(round+1) +',DEPOT-'+str(id_depot)+','+ 'Generation:' + str(gen + 1) + ' #########################')
+                # print('This Gen Distance :' + str(10000.0 / current_fitness))
+
+            ### Evaluate results of current DEPOT ####
+            fitness_table = []
+            for chromosome in populations:
+                fitness_table.append(
+                    evaluate.chromosomeFitness(chromosome, DISTANCES, DISTANCES_FROM_DEPOTS, DISTANCES_TO_DEPOTS, id_depot))
+            populations = [x for _, x in sorted(zip(fitness_table, populations), reverse=True)]
+            # print("GA time --- %s seconds ---" % (time.time() - GA_time))
+            best_chromosome = populations[0]
+            computational_time = time.time() - calculation_time
+            # print("Total Calculation time --- %s seconds ---" % (computational_time))
+            distance = evaluate.chromosomeRoutesDistance(best_chromosome,DISTANCES,DISTANCES_FROM_DEPOTS,DISTANCES_TO_DEPOTS,id_depot)
+            # print('Distances of the best chromosome: ' + str(distance))
+            # print(best_chromosome)
+
+            ## Memo the results ##
+            results_all_depots.append(best_chromosome)
+            distance_all_depots.append(distance)
+            time_all_depots.append(computational_time)
+        total_results.append((results_all_depots,distance_all_depots,time_all_depots))
+    return total_results
+
+
+def mean_results(results):
+    solutions = []
+    cal_times = []
+    distances = []
+    for round, round_result in enumerate(results):
+        sol = results[round][0]
+        dist = results[round][1]
+        ct = results[round][2]
+
+        solutions.append(sol)
+        distances.append(dist)
+        cal_times.append(ct)
+    distances = np.array(distances)
+    distances = pd.DataFrame(distances)
+    distances = np.mean(distances, axis=0)
+    cal_times = np.array(cal_times)
+    cal_times = pd.DataFrame(cal_times)
+    cal_times = np.mean(cal_times, axis=0)
+    return distances, cal_times
+
+######################### MAIN ########################################################
+
+
+path = 'pdp_instances/LiLim/doing/'
+FILENAMES = next(os.walk(path))[2]
+
+all_results = []
+
+for name in FILENAMES:
+    filepath = path + name
+    results = solve_and_print(filepath)
+    distances,cal_times = mean_results(results)
+    all_results.append((name,distances,cal_times))
+
+
+distances,cal_times = mean_results(results)
+
+dist = [dist for _,dist,_ in all_results]
+dist = np.array(dist)
+df = pd.DataFrame(dist)
+df.to_csv('distances_df.csv')
+
+CT = [ct for _,_,ct in all_results]
+CT = np.array(CT)
+df_CT = pd.DataFrame(CT)
+df_CT.to_csv('CT_df.csv')
+
